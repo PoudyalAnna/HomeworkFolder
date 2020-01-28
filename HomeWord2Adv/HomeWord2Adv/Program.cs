957a091328015b7a1bab2822e5f53736c1b683f6
Anita <52110800+PoudyalAnna@users.noreply.github.com>
2020-01-28 15:22:23 -0600

    next change

--- HomeWord2Adv/HomeWord2Adv/Program.cs
+++ HomeWord2Adv/HomeWord2Adv/Program.cs
@@ -7,10 +7,43 @@ using System.Threading.Tasks;
 namespace HomeWord2Adv
 {
     class Program
+
     {
-        static void Main(string[] args)
+        
+        static int GetGCD(int num1, int num2)
         {
-            //this is conncetion.
+            while (num1 != num2)
+            {
+                if (num1 > num2)
+                    num1 = num1 - num2;
+
+                if (num2 > num1)
+                    num2 = num2 - num1;
+            }
+            return num1;
+        }   
+        
+
+        static void Main(string[] args)
+
+        {
+        
+            Console.WriteLine("C# Program for  GCD");
+
+            Console.Write("Enter First Number: ");
+           int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+         int b = Convert.ToInt32(Console.ReadLine());
+
+           
+
+           int gcd = GetGCD(a, b);           
+
+            Console.WriteLine("\nGCD ({0,4},{1,4}) = {2,6} ", a, b,gcd);
+
+           
+            Console.ReadLine();
 
         }
     }
